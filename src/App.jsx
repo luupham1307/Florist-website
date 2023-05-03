d74651509191539e9b72a678874ea30a191ff880
nghiemtrang <125233841+nghiemtrang@users.noreply.github.com>
2023-05-04 00:03:00 +0700

    Add img in CallToAction

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Footer from "./common/layout/Footer/Footer";
 import Instagram from "./common/layout/Instagram";
 import Posts from "./modules/home/pages/Posts";
+import CallToAction from "./modules/home/pages/CallToAction";
 
 export default function App() {
   return (
     <React.Fragment>
+      <CallToAction/>
       {/* <Posts/> */}
       <Instagram />
       <Footer />
---
+++ src/modules/home/pages/CallToAction.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ContainerFuild from "../../../common/components/ContainerFuild";
+
+export default function CallToAction() {
+  return (
+    <div>
+      <ContainerFuild>
+        <div>
+            <img src="https://zjuxinjcqrcvncurfkwx.supabase.co/storage/v1/object/public/Image/Slides/call-bg.jpg" alt="" />
+            <span></span>
+            <h2></h2>
+            <button></button>
+            <button></button>
+        </div>
+      </ContainerFuild>
+    </div>
+  );
+}
