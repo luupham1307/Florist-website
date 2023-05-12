73ee7b6be38608fd6bc8ed2f775cf7a6228cc10c
luupham1307 <hoangluupham1307@gmail.com>
2023-05-12 21:54:23 +0700

    add my team

---
+++ src/modules/about/pages/Services.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Out_Services() {
+    return (
+        <div className='text-center mb-[75px]'>
+            <span className="font-[Quicksand] text-[#f45d96] text-base font-bold uppercase tracking-[4px] pb-2 ">
+                Our services
+            </span>
+            <h2 className="font-[Libre Baskerville] text-h2 leading-[58px] text-[#111111] font-normal ">
+                Florist’s services
+            </h2>
+        </div>
+    )
+}
