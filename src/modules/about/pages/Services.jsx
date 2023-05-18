f199780ce44172daf81dbef24cf4f0269924188f
luupham1307 <hoangluupham1307@gmail.com>
2023-05-18 20:53:29 +0700

    delete contact

--- src/modules/about/pages/Services.jsx
+++ src/modules/about/pages/Services.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import ServiceType from '../../../common/components/ServiceType'
 
 export default function Out_Services() {
     return (
-        <div className='text-center mb-[75px]'>
-            <span className="font-[Quicksand] text-[#f45d96] text-base font-bold uppercase tracking-[4px] pb-2 ">
-                Our services
-            </span>
-            <h2 className="font-[Libre Baskerville] text-h2 leading-[58px] text-[#111111] font-normal ">
-                Florist’s services
-            </h2>
-        </div>
+       
+            <div className='text-center mb-[75px]'>
+                <span className="font-[Quicksand] text-[#f45d96] text-base font-bold uppercase tracking-[4px] pb-2 ">
+                    Our services
+                </span>
+                <h2 className="font-[Libre Baskerville] text-h2 leading-[58px] text-[#111111] font-normal ">
+                    Florist’s services
+                </h2>
+            </div>
+            <ServiceType />
+
     )
 }
