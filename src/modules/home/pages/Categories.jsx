af4918cea5af2bed79c1c5e236b4830b549eabeb
hominhhieu7 <cuteophong@gmail.com>
2023-05-26 20:25:38 +0700

    fix categories

--- src/modules/home/pages/Categories.jsx
+++ src/modules/home/pages/Categories.jsx
@@ -1,27 +1,49 @@
-import { useEffect, useState } from "react"
+// import { useEffect, useState } from "react"
 import Container from "../../../common/components/Container"
-import axios from "axios";
+// import axios from "axios";
+let categories = [
+    {
+        imgUrl: "https://zjuxinjcqrcvncurfkwx.supabase.co/storage/v1/object/public/Image/Categories/categories-1.jpg",
+        title: "Fresh Flower",
+        item: "( 25 items )"
+    },
+    {
+        imgUrl: "https://zjuxinjcqrcvncurfkwx.supabase.co/storage/v1/object/public/Image/Categories/categories-2.jpg",
+        title: "Succulent plants",
+        item: "( 162 items )"
+    },
+    {
+        imgUrl: "https://zjuxinjcqrcvncurfkwx.supabase.co/storage/v1/object/public/Image/Categories/categories-3.jpg",
+        title: "Cactus plants",
+        item: "( 58 items )"
+    },
+    {
+        imgUrl: "https://zjuxinjcqrcvncurfkwx.supabase.co/storage/v1/object/public/Image/Categories/categories-4.jpg",
+        title: "Funiture treer",
+        item: "( 25 items )"
+    }
+]
 export default function Categories() {
-    const [categories, setCategories] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null)
-    useEffect(() => {
-        if (!categories.length) {
-            setIsLoading(true)
-            axios.get('http://18.141.139.55:3000/categories').then((res) => {
+    // const [categories, setCategories] = useState([]);
+    // const [isLoading, setIsLoading] = useState(false);
+    // const [error, setError] = useState(null)
+    // useEffect(() => {
+    //     if (!categories.length) {
+    //         setIsLoading(true)
+    //         axios.get('http://18.141.139.55:3000/categories').then((res) => {
 
-                setCategories(res.data.data)
-                setIsLoading(false);
-            }).catch(() => {
-                setIsLoading(false);
-                setError("error")
-            })
-        }
-    }, [categories])
+    //             setCategories(res.data.data)
+    //             setIsLoading(false);
+    //         }).catch(() => {
+    //             setIsLoading(false);
+    //             setError("error")
+    //         })
+    //     }
+    // }, [categories])
     return (
         <Container>
-            {isLoading && "Đang tải dữ liệu"}
-            {error && <div style={{ color: 'red' }}>Có lỗi khi tải dữ liệu!!!</div>}
+            {/* {isLoading && "Đang tải dữ liệu"}
+            {error && <div style={{ color: 'red' }}>Có lỗi khi tải dữ liệu!!!</div>} */}
 
             <div className="w-full  md:px-0 mx-auto  ">
 
